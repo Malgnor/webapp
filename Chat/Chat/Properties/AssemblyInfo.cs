8019ce0c9a78f1b4fa3c37ed2bfcc713994ce83f
unknown <alex.messias@LTCH0128.cwbdis.pucpr.br>
2015-10-14 22:12:09 -0300

    .

--- Chat/Chat/Properties/AssemblyInfo.cs
+++ Chat/Chat/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f6c106d5-9114-4f26-a6ac-6838d8e7b8d3")]
+[assembly: Guid("286d3f91-2590-4481-b4dc-f7269ab8d4de")]
 
 // Version information for an assembly consists of the following four values:
 //
